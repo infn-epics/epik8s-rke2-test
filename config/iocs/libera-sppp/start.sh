41f10f6beaa0611416c53e68ab1c72b8b3a6fe2d
amichelo <andrea.michelotti@lnf.infn.it>
2024-04-29 23:52:13 +0200

    fixed to support absolute paths

--- config/iocs/libera-sppp/start.sh
+++ config/iocs/libera-sppp/start.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 if [ -n "$__IOC_PREFIX__" ];then
     echo "patching __IOC_PREFIX__ $__IOC_PREFIX__ "
-    sed  -i s/__IOC_PREFIX__/$__IOC_PREFIX__/ *.substitutions *.cmd
+    sed  -i s/__IOC_PREFIX__/$__IOC_PREFIX__/g db/*.substitutions *.cmd
 else
     echo "%%Warning no __IOC_PREFIX__ defined"
 fi
 
 if [ -n "$__IOC_TOP__" ];then
     echo "patching __IOC_TOP__ $__IOC_TOP__ "
-    sed  -i s/__IOC_TOP__/$__IOC_TOP__/ *.substitutions *.cmd
+    sed  -i s|__IOC_TOP__|$__IOC_TOP__|g db/*.substitutions *.cmd
 else
     echo "%%Warning no __IOC_TOP__ defined"
 fi
 
 if [ -n "$__IOC_NAME__" ];then
     echo "patching __IOC_NAME__ $__IOC_NAME__ "
-    sed  -i s/__IOC_NAME__/$__IOC_NAME__/ *.substitutions *.cmd
+    sed  -i s/__IOC_NAME__/$__IOC_NAME__/g db/*.substitutions *.cmd
 else
     echo "%%Warning no __IOC_NAME__ defined"
 fi
