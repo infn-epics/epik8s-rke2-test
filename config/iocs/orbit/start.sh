3c5fdcd18554b8df7b3be8c26c63fe1cf7b278c7
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-03 12:09:47 +0200

    debug

--- config/iocs/orbit/start.sh
+++ config/iocs/orbit/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 script_dir=$(dirname "$0")
 cd $script_dir
-echo "Starting Accumulator Orbit addr: $EPICS_CA_ADDR_LIST"
+echo "Starting Orbit addr: $EPICS_CA_ADDR_LIST"
 python ./scripts/py-ioc-collector.py -c sparc_orbit.json
+echo "Exited..."
