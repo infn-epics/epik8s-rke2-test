c0cc00e04a21921bcfd84dd3d43b37e73ce6e157
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-03 12:18:40 +0200

    less debug

--- config/services/cagateway/start.sh
+++ config/services/cagateway/start.sh
@@ -2,4 +2,4 @@
 name=`hostname`
 echo "Starting CA Gateway $EPICS_CA_ADDR_LIST on $EPICS_GATEWAY_ADDR_LIST ($name)"
 
-/epics/ca-gateway/bin/linux-x86_64/gateway -cip "$EPICS_CA_ADDR_LIST" -pvlist /epics/ca-gateway/configure/pvlist -debug 10 -sip 0.0.0.0 -access /epics/ca-gateway/configure/gwaccess.access
+/epics/ca-gateway/bin/linux-x86_64/gateway -cip "$EPICS_CA_ADDR_LIST" -pvlist /epics/ca-gateway/configure/pvlist -debug 1 -sip 0.0.0.0 -access /epics/ca-gateway/configure/gwaccess.access
