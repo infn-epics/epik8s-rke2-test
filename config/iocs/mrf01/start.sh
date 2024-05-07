71ac74a9e7779d6e289eee4e1f0661979131115e
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-07 14:40:45 +0200

    modprobe mrf

--- config/iocs/mrf01/start.sh
+++ config/iocs/mrf01/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "iocname:$__IOC_PREFIX__" 
+modprobe mrf
+
 if [ -n "$__IOC_PREFIX__" ];then
     echo "$__IOC_PREFIX__ patching "
     sed  -i s/__IOC_PREFIX__/$__IOC_PREFIX__/ *.substitutions *.cmd envPaths *.db
