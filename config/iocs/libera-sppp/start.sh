89a42b6d4329b22525bf77803b4c966b835a6f1d
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-21 16:10:18 +0200

    try not explicit replace

--- config/iocs/libera-sppp/start.sh
+++ config/iocs/libera-sppp/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mount -o remount ,rw /
-echo "server 193.206.84.121" > /openntpd/ntpd.conf
-echo "server 193.206.84.122" >> /openntpd/ntpd.conf
-echo "server 193.206.84.123" >> /openntpd/ntpd.conf
+echo "server 193.206.84.121" > /etc/openntpd/ntpd.conf
+echo "server 193.206.84.122" >> /etc/openntpd/ntpd.conf
+echo "server 193.206.84.123" >> /etc/openntpd/ntpd.conf
 ntpd -s
 /etc/init.d/libera restart
 
