3a04e32b9d5f9b77b2b4c95576b410540c520226
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-22 15:54:00 +0200

    motor interface updated to Phoebus and initialized from file

--- config/iocs/libera-sppp/start.sh
+++ config/iocs/libera-sppp/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mount -o remount ,rw /
+mount -o remount,rw /
 echo "server 193.206.84.121" > /etc/openntpd/ntpd.conf
 echo "server 193.206.84.122" >> /etc/openntpd/ntpd.conf
 echo "server 193.206.84.123" >> /etc/openntpd/ntpd.conf
