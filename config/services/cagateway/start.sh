021b677d495bbb2936038261f4ac66961b78aa64
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-22 20:02:41 +0200

    gateway disconnect timeout 60s

--- config/services/cagateway/start.sh
+++ config/services/cagateway/start.sh
@@ -2,4 +2,4 @@
 name=`hostname`
 echo "Starting CA Gateway $EPICS_CA_ADDR_LIST on $EPICS_GATEWAY_ADDR_LIST ($name)"
 # -debug 1 
-/epics/ca-gateway/bin/linux-x86_64/gateway -cip "$EPICS_CA_ADDR_LIST" -pvlist /epics/ca-gateway/configure/pvlist -sip 0.0.0.0 -access /epics/ca-gateway/configure/gwaccess.access
+/epics/ca-gateway/bin/linux-x86_64/gateway -cip "$EPICS_CA_ADDR_LIST" -pvlist /epics/ca-gateway/configure/pvlist -disconnect_timeout 60 -sip 0.0.0.0 -access /epics/ca-gateway/configure/gwaccess.access
