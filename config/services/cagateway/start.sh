837e12ab4d42f3961b6dcc8ae8236348c618efc1
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-22 18:22:00 +0200

    use balancer with 2 instances and increase timeout of the gateway

--- config/services/cagateway/start.sh
+++ config/services/cagateway/start.sh
@@ -2,4 +2,5 @@
 name=`hostname`
 echo "Starting CA Gateway $EPICS_CA_ADDR_LIST on $EPICS_GATEWAY_ADDR_LIST ($name)"
 # -debug 1 
+export EPICS_CA_CONN_TMO=120
 /epics/ca-gateway/bin/linux-x86_64/gateway -cip "$EPICS_CA_ADDR_LIST" -pvlist /epics/ca-gateway/configure/pvlist -sip 0.0.0.0 -access /epics/ca-gateway/configure/gwaccess.access
