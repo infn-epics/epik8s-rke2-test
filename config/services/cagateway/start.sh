f4f3653d6e309eb5e31808aa042f888eb89ab69f
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-26 22:02:37 +0200

    try gatway with shorter timeouts

--- config/services/cagateway/start.sh
+++ config/services/cagateway/start.sh
@@ -2,4 +2,6 @@
 name=`hostname`
 echo "Starting CA Gateway $EPICS_CA_ADDR_LIST on $EPICS_GATEWAY_ADDR_LIST ($name)"
 # -debug 1 
-/epics/ca-gateway/bin/linux-x86_64/gateway -cip "$EPICS_CA_ADDR_LIST" -pvlist /epics/ca-gateway/configure/pvlist -disconnect_timeout 60 -sip 0.0.0.0 -access /epics/ca-gateway/configure/gwaccess.access
+defaults="-connect_timeout 30 -inactive_timeout 300 -dead_timeout 300 -disconnect_timeout 30 -reconnect_inhibit 300"
+options="-dead_timeout 10 -disconnect_timeout 5 -reconnect_inhibit 2 -connect_timeout 5"
+/epics/ca-gateway/bin/linux-x86_64/gateway -cip "$EPICS_CA_ADDR_LIST" -pvlist /epics/ca-gateway/configure/pvlist $options -sip 0.0.0.0 -access /epics/ca-gateway/configure/gwaccess.access
