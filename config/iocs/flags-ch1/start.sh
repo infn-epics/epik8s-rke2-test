ec682435541f426661396432b07386e820520a1e
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-27 14:08:22 +0200

    fix path requirements

--- config/iocs/flags-ch1/start.sh
+++ config/iocs/flags-ch1/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_dir=$(dirname "$0")
 cd $script_dir
-pip install -r requirements.txt
+pip install -r scripts/requirements.txt
 echo "Starting __IOC_NAME__: $EPICS_CA_ADDR_LIST"
 python ./scripts/pymotflag.py sparc-config.yaml
 echo "Exited..."
