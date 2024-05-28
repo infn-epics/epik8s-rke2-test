0bffbe3858673c22b957666dc95ad2eb9393d471
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-28 15:16:44 +0200

    reserve more memory set gateway prefix and restore default params

--- config/services/cagateway/start.sh
+++ config/services/cagateway/start.sh
@@ -6,8 +6,9 @@ echo "Starting caRepeater"
 
 /epics/epics-base/bin/linux-x86_64/caRepeater&
 sleep 1
-defaults="-connect_timeout 30 -inactive_timeout 300 -dead_timeout 300 -disconnect_timeout 30 -reconnect_inhibit 300"
-options="-dead_timeout 10 -disconnect_timeout 5 -reconnect_inhibit 2 -connect_timeout 5"
+defaults="-connect_timeout 30 -inactive_timeout 300 -dead_timeout 300 -disconnect_timeout 30 -reconnect_inhibit 300 -prefix gateway"
+#options="-dead_timeout 10 -disconnect_timeout 5 -reconnect_inhibit 2 -connect_timeout 5 -prefix gateway"
+options=$defaults
 echo "Gateway options: $options"
 
 /epics/ca-gateway/bin/linux-x86_64/gateway -cip "$EPICS_CA_ADDR_LIST" -pvlist /epics/ca-gateway/configure/pvlist $options -sip 0.0.0.0 -access /epics/ca-gateway/configure/gwaccess.access
