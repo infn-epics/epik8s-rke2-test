b32ca2c59ad3e74d2c27f9cd8eb9294597566027
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-28 12:27:52 +0200

    typo sh syntax

--- config/services/cagateway/start.sh
+++ config/services/cagateway/start.sh
@@ -5,7 +5,7 @@ echo "Starting CA Gateway $EPICS_CA_ADDR_LIST on $EPICS_GATEWAY_ADDR_LIST ($name
 echo "Starting caRepeater"
 
 /epics/epics-base/bin/linux-x86_64/caRepeater&
-sleep(1)
+sleep 1
 defaults="-connect_timeout 30 -inactive_timeout 300 -dead_timeout 300 -disconnect_timeout 30 -reconnect_inhibit 300"
 options="-dead_timeout 10 -disconnect_timeout 5 -reconnect_inhibit 2 -connect_timeout 5"
 echo "Gateway options: $options"
