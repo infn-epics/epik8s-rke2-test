ee6d275c2c2546ba3358f5dd2d463b8e1143acca
amichelo <andrea.michelotti@lnf.infn.it>
2024-06-03 23:39:14 +0200

    re-test gateway

--- config/services/cagateway/start.sh
+++ config/services/cagateway/start.sh
@@ -7,7 +7,7 @@ unset EPICS_CAS_BEACON_ADDR_LIST
 unset EPICS_CAS_AUTO_BEACON_ADDR_LIST
 /epics/epics-base/bin/linux-x86_64/caRepeater&
 sleep 1
-defaults="-connect_timeout 600 -inactive_timeout 300 -dead_timeout 300 -disconnect_timeout 10 -reconnect_inhibit 60"
+defaults="-debug 10 -connect_timeout 5 -inactive_timeout 300 -dead_timeout 60 -disconnect_timeout 10 -reconnect_inhibit 60 -signore gateway-0,gateway-1"
 #options="-dead_timeout 10 -disconnect_timeout 5 -reconnect_inhibit 2 -connect_timeout 5 -prefix gateway"
 options=$defaults
 echo "Gateway options: $options"
