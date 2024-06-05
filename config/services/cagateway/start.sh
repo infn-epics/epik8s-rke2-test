eb4a2e678e3ebffefa7b2fc0b873975f7b437d9a
amichelo <andrea.michelotti@lnf.infn.it>
2024-06-05 18:00:03 +0200

    cagateway

--- config/services/cagateway/start.sh
+++ config/services/cagateway/start.sh
@@ -5,9 +5,10 @@ echo "Starting CA Gateway $EPICS_CA_ADDR_LIST on $EPICS_GATEWAY_ADDR_LIST ($name
 echo "Starting caRepeater"
 export EPICS_CAS_BEACON_ADDR_LIST=$EPICS_CA_ADDR_LIST
 export EPICS_CAS_AUTO_BEACON_ADDR_LIST=NO
+export EPICS_CAS_IGNORE_ADDR_LIST="cagateway-0 cagateway-1"
 /epics/epics-base/bin/linux-x86_64/caRepeater&
 sleep 1
-defaults="-connect_timeout 5 -inactive_timeout 300 -dead_timeout 60 -disconnect_timeout 10 -reconnect_inhibit 60 -signore cagateway-0,cagateway-1"
+defaults="-connect_timeout 5 -inactive_timeout 300 -dead_timeout 60 -disconnect_timeout 10 -reconnect_inhibit 60"
 #options="-dead_timeout 10 -disconnect_timeout 5 -reconnect_inhibit 2 -connect_timeout 5 -prefix gateway"
 options=$defaults
 echo "Gateway options: $options"
