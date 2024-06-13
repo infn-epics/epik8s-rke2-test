f3b71b52275a4307fd17c1437f626728ae2eef50
amichelo <andrea.michelotti@lnf.infn.it>
2024-06-13 12:59:04 +0200

    dump sppp BPM pvs

--- config/iocs/libera-sppp/start.sh
+++ config/iocs/libera-sppp/start.sh
@@ -6,18 +6,5 @@ echo "server 193.206.84.123" >> /etc/openntpd/ntpd.conf
 ntpd -s
 /etc/init.d/libera restart
 
-# variables=( "__IOC_PREFIX__" "__IOC_TOP__" "__IOC_NAME__" "__BPM1__" "__BPM2__" "__BPM3__" "__BPM4__")
-
-# # Loop through each variable
-# for var in "${variables[@]}"; do
-#     # Check if the variable is defined and non-empty
-#     if [ -n "${!var}" ]; then
-#         echo "patching $var ${!var}"
-#         # Use sed to replace the variable in the specified files
-#         sed -i "s|$var|${!var}|g" db/* *.cmd
-#     else
-#         echo "%%Warning no $var defined"
-#     fi
-# done
 
 ./st.cmd
