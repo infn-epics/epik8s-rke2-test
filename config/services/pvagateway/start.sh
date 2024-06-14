a9168cc0e61cb4f966d5892a5ecd0bdacc387a14
amichelo <andrea.michelotti@lnf.infn.it>
2024-06-14 12:31:28 +0200

    try to serve pva

---
+++ config/services/pvagateway/start.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+name=`hostname`
+echo "Starting PVA Gateway $EPICS_CA_ADDR_LIST on $EPICS_GATEWAY_ADDR_LIST ($name)"
+# -debug 1
+CLIENT_NAME="$name"
+
+# Generate JSON content
+JSON_CONTENT=$(cat <<EOF
+{
+    "version": 2,
+    "clients":[
+        {
+            "name":"$CLIENT_NAME",
+            "autoaddrlist":false
+        }
+    ],
+    "servers": [
+        {
+            "name": "$SERVER_NAME",
+            "clients": ["$CLIENT_NAME"],
+            "addrlist": "$EPICS_CA_ADDR_LIST",
+            "autoaddrlist": false,
+            "statusprefix": "GW:STS:"
+        }
+    ]
+}
+EOF
+)
+
+# Output the JSON content to a file
+echo "$JSON_CONTENT" > gateway_config.json
+
+echo "Generated gateway_config.json:"
+
+python3 -m p4p.gw --debug gateway_config.json
