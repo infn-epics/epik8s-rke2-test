148a8add65849126be5b0ad0529309e3716927ee
amichelo <andrea.michelotti@lnf.infn.it>
2024-06-24 00:29:12 +0200

    every 12 hours

--- config/cronjobs/chfeeder/start.sh
+++ config/cronjobs/chfeeder/start.sh
@@ -3,6 +3,6 @@ script_dir=$(dirname "$0")
 cd $script_dir
 pip install -r /app/requirements.txt
 echo "Starting Channel Finder feeder, looking __CONFIG_DIR__ http://sparc-channelfinder.apps.okd-datest.lnf.infn.it/ChannelFinder epics epics"
-
-python /app/cfeeder.py __CONFIG_DIR__ http://sparc-channelfinder.apps.okd-datest.lnf.infn.it/ChannelFinder epics epics
+export EPICS_CA_ADDR_LIST=$EPICS_GATEWAY
+python /app/cfeeder.py __CONFIG_DIR__ http://sparc-channelfinder.apps.okd-datest.lnf.infn.it/ChannelFinder admin adminPass
 echo "Exited..."
