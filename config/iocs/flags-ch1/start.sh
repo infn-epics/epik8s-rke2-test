23ffdb7d10ce684bf87c2f1e537748d591431f9f
amichelo <andrea.michelotti@lnf.infn.it>
2024-07-02 18:14:33 +0200

    adding dump of pvs

--- config/iocs/flags-ch1/start.sh
+++ config/iocs/flags-ch1/start.sh
@@ -3,5 +3,5 @@ script_dir=$(dirname "$0")
 cd $script_dir
 pip install -r scripts/requirements.txt
 echo "Starting __IOC_NAME__"
-python ./scripts/pymotflag.py sparc-config.yaml
+python ./scripts/pymotflag.py sparc-config.yaml -p __CONFIG_DIR__/pvlist.txt
 echo "Exited..."
