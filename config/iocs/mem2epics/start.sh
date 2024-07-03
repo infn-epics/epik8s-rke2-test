d9f0aed996da362f8378b4901f49a821ea2245e3
amichelo <andrea.michelotti@lnf.infn.it>
2024-07-03 18:27:37 +0200

    adding VAC

--- config/iocs/mem2epics/start.sh
+++ config/iocs/mem2epics/start.sh
@@ -2,4 +2,4 @@
 script_dir=$(dirname "$0")
 cd $script_dir
 echo "Starting $__IOC_NAME__ : $EPICS_CA_ADDR_LIST"
-python ./scripts/laser_synch_softIOC.py -c laser_synch.json
+python ./scripts/mem2epics.py -i __IOC_PREFIX__ -c __CONFIGURATION__ -p __CONFIG_DIR__/pvlist.txt -ms __MEMECACHED_SERVER__
