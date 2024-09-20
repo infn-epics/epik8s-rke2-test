7600e4d08f67c5a67eb856208faf059f3470e840
amichelo <andrea.michelotti@lnf.infn.it>
2024-09-20 14:38:30 +0200

    adding cameras

--- config/iocs/mem2epics/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-script_dir=$(dirname "$0")
-cd $script_dir
-pip install -r scripts/requirements.txt
-
-echo "Starting $__IOC_NAME__ : $EPICS_CA_ADDR_LIST"
-python ./scripts/mem2epics.py -i __IOC_PREFIX__ -c __CONFIGURATION__ -p __CONFIG_DIR__/pvlist.txt -ms __MEMECACHED_SERVER__
