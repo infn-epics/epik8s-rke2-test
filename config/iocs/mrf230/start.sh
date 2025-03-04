9e19df871570b3e58287e92596460e060ca1a625
amichelo <andrea.michelotti@lnf.infn.it>
2025-03-04 10:18:19 +0100

    updated unimag

--- config/iocs/mrf230/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "iocname:$__IOC_PREFIX__"
-./st.cmd
--- config/iocs/mrf230/tmg-EVRinput.cmd
+++
@@ -1,9 +0,0 @@
-#############################################
-## EVR event on external input trigger     ##
-#############################################
-
-## Set EVR to emit event "CODE" on external trigger on input INP
-dbpf("$(PREFIX)-$(EVR):FPIn$(INP)-Edge-Sel","Active Rising")
-dbpf("$(PREFIX)-$(EVR):FPIn$(INP)-Trig-Ext-Sel","Edge")
-
-
